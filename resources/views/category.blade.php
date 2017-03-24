59a8198ad330814c7ac5b1c524664ab2a060d53a
student <student@Win64-44.ue.loc>
2017-03-24 14:16:47 +0100

    links

---
+++ resources/views/category.blade.php
@@ -0,0 +1,9 @@
+<h4>{{$category->name}}</h4>
+<ul>
+    @foreach($category->articles as $article)
+    <li> {{$article->title}}</li>
+    @endforeach
+    </ul>
+
+<textarea name ="content" rows="3"></textarea>
+    
